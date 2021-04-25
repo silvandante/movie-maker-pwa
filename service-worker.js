48488e799e87591e90ac188a3161c36070a5cb28
Anny Walker <wscarol@hotmail.com>
2021-04-25 13:59:30 -0400

    fixing claim

--- service-worker.js
+++ service-worker.js
@@ -42,19 +42,7 @@ self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Ativando...');
 
     evt.waitUntil(
-
-        caches.keys().then((keylist) => {
-
-            return Promise.all(keylist.map((key) => {
-
-                //console.log('[ServiceWorker] Removendo cache antigo...');
-                if(key !== CACHE_NAME){
-                    return caches.delete(key);
-                }
-            }));
-        }).then(() => {
-            self.clients.claim();
-        })
+            self.clients.claim()
     );
     
 });
